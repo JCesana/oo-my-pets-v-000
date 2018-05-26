7f355cc24171d56debcdc00e733c6dcbc17be6a2
Josh Cesana <joshuacesana@gmail.com>
2018-05-26 02:32:54 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,32 +9,39 @@ class Owner
     @pets = {:fishes => [], :dogs => [], :cats => []}
   end 
   
+  
   def buy_fish(name)
     fish = Fish.new(name)
     @pets[:fishes] << fish
   end 
+  
   
   def buy_cat(name)
     cat = Cat.new(name)
     @pets[:cats] << cat 
   end 
   
+  
   def buy_dog(name)
     dog = Dog.new(name)
     @pets[:dogs] << dog
   end 
   
+  
   def walk_dogs
     @pets[:dogs].each { |dog| dog.mood = "happy" }
   end 
+  
   
   def play_with_cats
     @pets[:cats].each { |cat| cat.mood = "happy" }
   end 
   
+  
   def feed_fish
     @pets[:fishes].each { |fish| fish.mood = "happy" }  
   end 
+  
   
   def sell_pets
     @pets[:dogs].each { |dog| dog.mood = "nervous" }
@@ -43,22 +50,32 @@ class Owner
     @pets[:dogs].clear
     @pets[:cats].clear
     @pets[:fishes].clear
+  end 
+  
+  
+  def list_pets
     
   end 
+  
+  
   def say_species
     "I am a #{@species}."
   end 
+
 
   def self.all
     @all
   end 
   
+  
   def self.count 
     @count 
   end 
+  
   
   def reset_all
     @@all = []
     @@count = 0 
   end 
+  
 end
